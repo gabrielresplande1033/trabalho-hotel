e7583fe137c47bee52d8edc5a53673f88946d22a
gabriel <gblmarcelino@gmail.com>
2018-12-03 17:55:11 -0300

    ajustando arquivos html e criando as migrations do bd

---
+++ app/Models/Hotel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gabriel
+ * Date: 03/12/18
+ * Time: 16:01
+ */
+
+namespace App\Models;
+
+
+class Hotel
+{
+
+}
---
+++ resources/views/hotel.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gabriel
+ * Date: 03/12/18
+ * Time: 13:08
+ */
